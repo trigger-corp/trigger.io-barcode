98144eda1e8b922c5104295ec157ae9cfd219397
Antoine van Gelder <antoine@trigger.io>
2020-01-28 14:49:49 +0200

    Removed dependency on external app for scanning barcodes on Android

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/barcode/API.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/barcode/API.java
@@ -1,81 +1,45 @@
 package io.trigger.forge.android.modules.barcode;
 
+import android.app.Activity;
+import android.content.Intent;
+
 import io.trigger.forge.android.core.ForgeApp;
 import io.trigger.forge.android.core.ForgeIntentResultHandler;
 import io.trigger.forge.android.core.ForgeLog;
 import io.trigger.forge.android.core.ForgeTask;
 
-import java.util.List;
-
 import com.google.gson.JsonObject;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ActivityNotFoundException;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
 
 public class API {
-    private static String intent_name = "com.google.zxing.client.android.SCAN";
-    private static String bs_package = "com.srowen.bs.android";
 
     public static void scan(final ForgeTask task) {
-        Intent intent = new Intent(intent_name);
-        intent.addCategory(Intent.CATEGORY_DEFAULT);
-        intent.setPackage(bs_package);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-        final PackageManager packageManager = ForgeApp.getActivity().getPackageManager();
-        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-        if (list != null && list.size() > 0) {
-            // Barcode scanner
-            ForgeApp.intentWithHandler(intent, new ForgeIntentResultHandler() {
-                @Override
-                public void result(int requestCode, int resultCode, Intent data) {
-                    // Result of scan
-                    if (resultCode == Activity.RESULT_OK) {
-                        JsonObject result = new JsonObject();
-                        result.addProperty("value", data.getStringExtra("SCAN_RESULT"));
-                        result.addProperty("format", data.getStringExtra("SCAN_RESULT_FORMAT"));
-                        task.success(result);
-                    } else if (resultCode == Activity.RESULT_CANCELED) {
-                        task.error("User cancelled", "EXPECTED_FAILURE", null);
-                    } else {
-                        task.error("Unknown error", "UNEXPECTED_FAILURE", null);
-                    }
+        IntentIntegrator integrator = new IntentIntegrator(ForgeApp.getActivity());
+
+        Intent intent = integrator.createScanIntent();
+        ForgeApp.intentWithHandler(intent, new ForgeIntentResultHandler() {
+            @Override
+            public void result(int requestCode, int resultCode, Intent data) {
+                if (resultCode == Activity.RESULT_CANCELED) {
+                    task.error("User cancelled", "EXPECTED_FAILURE", null);
+                    return;
                 }
-            });
-        } else {
-            // No barcode scanner
-            task.performUI(new Runnable() {
-                public void run() {
-                    AlertDialog.Builder downloadDialog = new AlertDialog.Builder(ForgeApp.getActivity());
-                    downloadDialog.setTitle("Install Barcode Scanner?");
-                    downloadDialog.setMessage("This application requires Barcode Scanner+. Would you like to install it?");
-                    downloadDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Uri uri = Uri.parse("market://details?id=" + bs_package);
-                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                            try {
-                                ForgeApp.getActivity().startActivity(intent);
-                                task.error("Barcode scanner unavailable, opening market for user to install.", "UNAVAILABLE", null);
-                            } catch (ActivityNotFoundException anfe) {
-                                ForgeLog.w("Android Market is not installed; cannot install Barcode Scanner");
-                                task.error("Barcode scanner unavailable", "UNAVAILABLE", null);
-                            }
-                        }
-                    });
-                    downloadDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            task.error("Barcode scanner unavailable", "UNAVAILABLE", null);
-                        }
-                    });
-                    downloadDialog.show();
+
+                IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
+                if (result == null || result.getContents() == null) {
+                    task.error("Unknown error", "UNEXPECTED_FAILURE", null);
+                    return;
+
                 }
-            });
-        }
+
+                JsonObject ret = new JsonObject();
+                ret.addProperty("value", result.getContents());
+                ret.addProperty("format", result.getFormatName());
+                task.success(ret);
+            }
+        });
     }
+
 }
